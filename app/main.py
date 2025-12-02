7de60e95bcc23895b7e3c35e4ef143f075a01bb3
Aaron Davila Santos <aaron.2000.539@gmailcom>
2025-12-02 15:33:47 -0500

    Se corrigio el archivo main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from typing import List, Optional
+from typing import List
 
 app = FastAPI(title="Docs Registry API", docs_url="/swagger")
 
